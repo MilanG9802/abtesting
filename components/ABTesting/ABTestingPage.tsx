92b8dfaba04285adc6c395fb030af8dc46f1db0d
Milan Golakiya TST <milangolakiya.tst@gmail.com>
2024-06-13 23:02:22 +0530

    middelware and userID

--- components/ABTesting/ABTestingPage.tsx
+++ components/ABTesting/ABTestingPage.tsx
@@ -3,9 +3,11 @@ import { useEffect, useState } from "react";
 import Card from "./Card";
 import { dispatch } from "@/store";
 import { setLoaded } from "@/store/reducer/loading";
+import { StatsigProvider } from "statsig-react";
 
 export default function ABTestingPage({ posts }: { posts: Card[] }) {
   const [abGroup, setAbGroup] = useState<string | null>("null");
+  const [userID, setUserID] = useState<number>(0);
 
   useEffect(() => {
     const group = document.cookie
@@ -13,28 +15,39 @@ export default function ABTestingPage({ posts }: { posts: Card[] }) {
       .find((row) => row.startsWith("ab_test_group="))
       ?.split("=")[1];
     setAbGroup(group || null);
+    const user = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("userID="))
+      ?.split("=")[1];
+    setUserID(user ? parseInt(user) : 0);
     dispatch(setLoaded(true));
   }, []);
 
   return (
-    <div>
-      {abGroup === "even" ? (
-        <Card
-          title={posts?.[0]?.title}
-          description={posts?.[0]?.description}
-          image={posts?.[0]?.thumbnail?.url}
-          color="black"
-        />
-      ) : abGroup === "odd" ? (
-        <Card
-          title={posts?.[1]?.title}
-          description={posts?.[1]?.description}
-          image={posts?.[1]?.thumbnail?.url}
-          color="grey"
-        />
-      ) : (
-        <Card title="" description="" image="" loading />
-      )}
-    </div>
+    <StatsigProvider
+      sdkKey={process.env.NEXT_PUBLIC_STATSIG_CLIENT_KEY!}
+      waitForInitialization={true}
+      user={{ userID }}
+    >
+      <div>
+        {abGroup === "even" ? (
+          <Card
+            title={posts?.[0]?.title}
+            description={posts?.[0]?.description}
+            image={posts?.[0]?.thumbnail?.url}
+            color="black"
+          />
+        ) : abGroup === "odd" ? (
+          <Card
+            title={posts?.[1]?.title}
+            description={posts?.[1]?.description}
+            image={posts?.[1]?.thumbnail?.url}
+            color="grey"
+          />
+        ) : (
+          <Card title="" description="" image="" loading />
+        )}
+      </div>
+    </StatsigProvider>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,20 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+let UserID = 1;
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
   const cookieName = "ab_test_group";
+  const userID = "userID";
 
   if (!request.cookies.get(cookieName)) {
-    const isEven = Math.random() >= 0.5;
-
-    response.cookies.set(cookieName, isEven ? "even" : "odd", {
+    response.cookies.set(cookieName, UserID % 2 === 0 ? "odd" : "even", {
       maxAge: 60 * 60 * 24 * 365,
     });
+    response.cookies.set(userID, `${UserID}`, {
+      maxAge: 60 * 60 * 24 * 365,
+    });
+    UserID++;
   }
-
+  console.log(UserID);
   return response;
 }
