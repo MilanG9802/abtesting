096dca71f5e1705ad80dc2bfcc83b41e6e927780
MilanGolakiya <milangolakiya.tst@gmail.com>
2024-06-13 18:26:19 +0530

    remote patters and card change

--- components/ABTesting/ABTestingPage.tsx
+++ components/ABTesting/ABTestingPage.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import Card from "./Card";
 
 export default function ABTestingPage() {
   const [abGroup, setAbGroup] = useState<string | null>(null);
@@ -19,15 +20,19 @@ export default function ABTestingPage() {
   return (
     <div>
       {abGroup === "even" ? (
-        <div>
-          <h1>Welcome to Group A</h1>
-          <p>This is the content for even-numbered users.</p>
-        </div>
+        <Card
+          title="Welcome to Group A"
+          description="This is the content for even-numbered users."
+          image="https://cdn.britannica.com/37/154237-050-A76A506D/blue-peafowl-tail-Indian-peacock-courtship-displays.jpg"
+          color="black"
+        />
       ) : (
-        <div>
-          <h1>Welcome to Group B</h1>
-          <p>This is the content for odd-numbered users.</p>
-        </div>
+        <Card
+          title="Welcome to Group B"
+          description="This is the content for odd-numbered users."
+          image="https://t3.ftcdn.net/jpg/05/56/88/18/360_F_556881864_T6KNWit9nKkRcgo7543KQQDJfIDvtocG.jpg"
+          color="grey"
+        />
       )}
     </div>
   );
--- components/ABTesting/Card.tsx
+++ components/ABTesting/Card.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React from "react";
+import styles from "./Card.module.css";
 
+// Define the Props interface
 interface CardProps {
   title?: string;
   description?: string;
@@ -8,22 +10,55 @@ interface CardProps {
   color?: string;
 }
 
+// Type the component
 const Card: React.FC<CardProps> = ({
   title = "",
   description = "",
   image = "",
-  color = "#000",
+  color = "",
 }) => {
+  const hoverClass =
+    color === "black" ? styles.blackHover : styles.lightGreyHover;
+
   return (
-    <div>
+    <div className={`${styles.card} ${hoverClass}`}>
       <p>{title}</p>
       <p>{description}</p>
-      <div>
+      <div className={styles.imageContainer}>
         <Image src={image} fill alt={title} />
       </div>
-      <p>{color}</p>
     </div>
   );
 };
 
 export default Card;
+
+// import Image from "next/image";
+// import React from "react";
+
+// interface CardProps {
+//   title?: string;
+//   description?: string;
+//   image?: string;
+//   color?: string;
+// }
+
+// const Card: React.FC<CardProps> = ({
+//   title = "",
+//   description = "",
+//   image = "",
+//   color = "#000",
+// }) => {
+//   return (
+//     <div>
+//       <p>{title}</p>
+//       <p>{description}</p>
+//       <div>
+//         <Image src={image} fill alt={title} />
+//       </div>
+//       <p>{color}</p>
+//     </div>
+//   );
+// };
+
+// export default Card;
